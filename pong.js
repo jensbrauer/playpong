818e1b01db303a8068e4d21fed5213498fb1fdf4
jensbrauer <henningsson.jens@gmail.com>
2022-12-13 18:39:02 +0000

    Created width and height variable in pong.js to set width and height of canvas in respons to loaded page size.

--- pong.js
+++ pong.js
@@ -1,9 +1,13 @@
+let container = document.getElementById('game-container')
+let canvasWidth = container.offsetWidth
+let canvasHeight = container.offsetHeight
+
 function setup() {
-    var canvas = createCanvas(400, 400);
+    var canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('game-container')
 }
   
 function draw() {
-    background(220);
-    
+    background(51);
+
 }

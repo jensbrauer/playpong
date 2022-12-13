9d7433694da2c5038458a953fa5700a61cc36ecf
jensbrauer <henningsson.jens@gmail.com>
2022-12-13 18:48:03 +0000

    Added onclick fucntionality in the game-container in orde rto initiate gameplay.

--- pong.js
+++ pong.js
@@ -6,8 +6,20 @@ function setup() {
     var canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('game-container')
 }
-  
+
+let gameRunning = false
+function runGame(){
+    gameRunning = true
+}
+
 function draw() {
     background(51);
+    if (gameRunning){
+        fill(200, 200, 200)
+        rect(200, 200, 50, 50)
+    } else {
+        fill(100, 100, 100)
+        rect(200, 200, 50, 50)
+    }
 
 }

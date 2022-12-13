d5b7dd84656d8eb029e20f35d52aee3743b9a40c
jensbrauer <henningsson.jens@gmail.com>
2022-12-13 19:51:30 +0000

    Created a ball that bounces between the rackets

--- pong.js
+++ pong.js
@@ -12,6 +12,9 @@ function runGame(){
     gameRunning = true
 }
 
+let ballPositionY = 55
+let ballGoingDown = true
+
 function draw() {
     background(51);
     if (gameRunning){
@@ -23,7 +26,23 @@ function draw() {
         fill(100, 100, 100)
         rect(mouseX, 5, 50, 25)
 
+        //ball
+        fill(100, 100, 100)
+        circle(mouseX, ballPositionY, 50, 25)
 
+
+        if (ballGoingDown) {
+            ballPositionY = ballPositionY + 2
+        } else {
+            ballPositionY = ballPositionY - 2
+        }
+
+        if (ballPositionY >= canvasHeight - 55){
+            ballGoingDown = false
+        } else if (ballPositionY <= 55 ){
+            ballGoingDown = true
+        }
+        
     } else {
         fill(100, 100, 100)
         circle(canvasWidth / 2, canvasHeight / 2, 50)
